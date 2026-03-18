38b397333fad21419d6287a61313ad95f77219d3
Rteix05 <rafaelteixeira0511@gmail.com>
2026-03-18 16:49:52 +0100

    font

---
+++ public/font/page.tsx
@@ -0,0 +1,5 @@
+use clientInformation;
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { Font } from "@prisma/client";
+import { getFonts } from "@/app/api/font/route";    
